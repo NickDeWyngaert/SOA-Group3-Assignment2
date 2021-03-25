9507d2cf795ae25723c752b8ada074fc6e1f2e8f
Reda Boussabat <reda.boussabat@hotmail.com>
2021-03-25 11:44:50 +0100

    verbetering van to_do_app

--- to_do_list/to_do_list_application/models.py
+++ to_do_list/to_do_list_application/models.py
@@ -5,7 +5,6 @@ from django.db import models
 class To_do_item(models.Model):
     name = models.CharField(max_length=100)
     course = models.CharField(max_length=50)
-    deadline = models.DateField()
     soort = models.CharField(max_length=50)
 
     def __str__(self):
