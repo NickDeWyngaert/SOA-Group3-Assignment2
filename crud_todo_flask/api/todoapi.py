f46b0ad21c44cbe6728ad275ce7752fc5d4c4113
Reda Boussabat <reda.boussabat@hotmail.com>
2021-05-05 20:07:00 +0200

    parameters aangepast

--- crud_todo_flask/api/todoapi.py
+++ crud_todo_flask/api/todoapi.py
@@ -19,23 +19,23 @@ todo_items = [
     }
 ]
 
-@app.route('/todo_items', methods=['GET'])
+@app.route('/todo', methods=['GET'])
 def get_todo_items():
     return jsonify({'todo_items': todo_items})
 
 
-@app.route('/todo_items/create', methods=['POST'])
+@app.route('/todo', methods=['POST'])
 def add_todo_item():
     todo_item = request.get_json(force=True)
     todo_items.append(todo_item)
     return jsonify({'todo_items': todo_items})
 
-@app.route('/todo_items/delete/<int:index>', methods=['DELETE'])
+@app.route('/todo/<int:index>', methods=['DELETE'])
 def delete_todo_item(index):
     todo_items.pop(index)
     return 'None', 200
 
-@app.route('/todo_items/update/<int:index>', methods=['PUT'])
+@app.route('/todo/<int:index>', methods=['PUT'])
 def update_todo_item(index):
     todo_item = request.get_json(force=True)
     todo_items[index] = todo_item
